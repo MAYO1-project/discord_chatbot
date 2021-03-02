baeedeb74761f27242156f3bcdebccb15f4215eb
leejun-ha <hiru0926@gmail.com>
2021-03-03 03:14:38 +0900

    change token

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,9 @@
 import discord
 import asyncio
 
-import settings
+import token_settings
 
-Settings = settings.Settings()
+Settings = token_settings.Settings()
 
 client = discord.Client()
 
@@ -15,5 +15,10 @@ async def on_ready():
     #change chatbot's game status
     game = discord.Game(name="명령 대기")
     await client.change_presence(status=discord.Status.idle, activity=game)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!music"):
+        await message.channel.send("검색하고 싶은 음악 제목을 입력해주세요!")
 
 client.run(Settings.token)
