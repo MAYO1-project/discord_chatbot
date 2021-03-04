971023938ab5f16252952c245ad054c956818f33
leejun-ha <hiru0926@gmail.com>
2021-03-05 03:18:10 +0900

    Add feature playing music through downloaded mp3 file

--- chatbot.py
+++ chatbot.py
@@ -11,12 +11,12 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("로그인 성공!!")
+    print("Login Success!!")
     print(client.user.name + "(%s)" %client.user.id)
 
     #change chatbot's game status
-    game = discord.Game(name="명령 대기")
-    await client.change_presence(status=discord.Status.idle, activity=game)
+    game = discord.Game(name="Waiting")
+    await client.change_presence(status=discord.Status.online, activity=game)
 
 @client.event
 async def on_message(message):
@@ -25,6 +25,7 @@ async def on_message(message):
         #await message.channel.send("재생하고 싶은 음악의 youtube url을 입력해주세요!")
         url = msg[1]
 
+        # download music from youtube url
         ydl_opts = {
             'format':'bestaudio/best',
             'postprocessors': [{
@@ -33,13 +34,31 @@ async def on_message(message):
                 'preferredquality': '320',
 
             }],
+            'outtmpl': 'song.%(ext)s',
         }
 
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
+
+        # bot join in user's voice channel
+        voice_channel = message.author.voice.channel
+        vc = await voice_channel.connect()
         
-        #channel = message.author.voice.channel
-        #await channel.connect()
-      
+        vc.play(discord.FFmpegPCMAudio('song.mp3'))
+
+        # change bot status to Music Playing
+        game = discord.Game(name="Music Playing♪")
+        await client.change_presence(status=discord.Status.online, activity=game)
+        await message.channel.send("Now playing~♪")
+
+''' # Finish music playing
+    if message.content == "!Fin":
+        voice_channel = message.author.voice.channel
+        #vc = await voice_channel.connect()
+        await vc.disconnect()
+        await message.channel.send("Leave this voice channel!")
         
+        game = discord.Game(name="Waiting")
+        await client.change_presence(status=discord.Status.online, activity=game)
+'''    
 client.run(Settings.token)
